a3fbfc5f3758d03e4e787b609d1ad20008b0863d
Gerald <mcamanor@gmail.com>
2026-04-20 19:22:37 +0000

    Update versioning and improve .gitignore for environment files
    - Bumped version numbers in app.config.js and package.json to 1.0.3 for consistency.
    - Enhanced .gitignore to ignore all environment files while allowing example templates for better security practices.

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: "kyoos",
     slug: "kyoos",
-    version: "1.0.1",
+    version: "1.0.3",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "kyoos",
@@ -32,8 +32,8 @@ module.exports = {
         light: "./assets/images/icon-light.png",
         tinted: "./assets/images/icon-tinted.png",
       },
-      buildNumber: "1.0.1",
-      versionCode: 2,
+      buildNumber: "1.0.3",
+      versionCode: 4,
     },
     android: {
       adaptiveIcon: {
@@ -47,8 +47,8 @@ module.exports = {
         "android.permission.CAMERA",
       ],
       package: "com.divsandviews.kyoos",
-      buildNumber: "1.0.1",
-      versionCode: 2,
+      buildNumber: "1.0.3",
+      versionCode: 4,
       googleServicesFile: "./google-services.json",
     },
     web: {
